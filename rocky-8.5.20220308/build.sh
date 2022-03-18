44eafe613103e3157e44d3b2645df769c9947871
stanleyguo0207 <stanleyguo0207@163.com>
2022-03-19 01:26:52 +0800

    feat: rocky-8.5.20220308 Dockerfile update.
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- rocky-8.5.20220308/build.sh
+++ rocky-8.5.20220308/build.sh
@@ -9,3 +9,6 @@ fi
 docker login
 docker pull rockylinux:8.5.20220308
 docker build --network=host --no-cache -f Dockerfile -t $1 .
+
+# ./build.sh dev_rocky:v8.5
+# docker run -it dev_rocky:v8.5 /bin/bash
