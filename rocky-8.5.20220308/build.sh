e0290a36f8e8bd64d31542fa35907755c9328239
stanleyguo0207 <stanleyguo0207@163.com>
2022-03-30 16:59:48 +0800

    oh-my-zsh update. feat:     1. oh-my-zsh update.     2. fonts.update.
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- rocky-8.5.20220308/build.sh
+++ rocky-8.5.20220308/build.sh
@@ -11,4 +11,4 @@ docker pull rockylinux:8.5.20220308
 docker build --network=host --no-cache -f Dockerfile -t $1 ../
 
 # ./build.sh dev_rocky:v8.5
-# docker run -it dev_rocky:v8.5 /bin/bash
+# docker run -it dev_rocky:v8.5 /bin/zsh
