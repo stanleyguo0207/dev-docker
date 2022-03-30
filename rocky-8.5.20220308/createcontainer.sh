bee19af2bc2b5645f88935d64f0448f8145e62a5
stanleyguo0207 <stanleyguo0207@163.com>
2022-03-31 01:29:21 +0800

    rocky-8.5 update. feat:     1. rocky-8.5 update.     2. createcontainer.sh and createuser.sh.
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- rocky-8.5.20220308/createcontainer.sh
+++ rocky-8.5.20220308/createcontainer.sh
@@ -12,22 +12,26 @@ else
 fi
 echo "script directory: $ScriptDir"
 
-if [ $# != 2]
+if [ $# != 4 ]
 then
-    echo "Usage: createcontainer.sh [container_name] [port]"
+    echo "Usage: createcontainer.sh [container_name] [port] [user_name] [user_id]"
     exit 1
 fi
 
 function CreateContainer(){
     docker create --name "$1"_dvc -v /opt/docker_home hello-world:latest
-    docker create --name "$1" --volumes-from "$1"_dvc -p "$2":22 --security-opt seccomp=unconfined --privileged=true $ImageName /usr/sbin/init
+    docker create --name "$1" --volumes-from "$1"_dvc -p "$2":22  --security-opt seccomp=unconfined --privileged=true $ImageName /usr/sbin/init
     docker start $1
     docker cp $ScriptDir/createuser.sh $1:/root
-    docker exec -it $1 /bin/zsh /root/createuser.sh $1 $2
-    echo "create root:root & "$1:$1" two accounts for container $1"
+    docker exec -it $1 /bin/zsh /root/createuser.sh $3 $4
+    echo "create root:root & "$3:$3" two accounts for container $1"
 }
 
 ContainerName=$1
 Port=$2
+UserName=$3
+UserId=$4
 
-CreateContainer $ContainerName $Port
+CreateContainer $ContainerName $Port $UserName $UserId
+
+# ./createcontainer.sh dev01 9901 stanleyguo0207 10001
--- rocky-8.5.20220308/createuser.sh
+++ rocky-8.5.20220308/createuser.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "$1 $2"
+UserName="$1"
+UserId="$2"
+useradd -m -d /opt/docker_home -u $UserId -G root -s /bin/zsh $UserName
+echo "root" | passwd --stdin root
+echo "$1" | passwd --stdin "$1"
