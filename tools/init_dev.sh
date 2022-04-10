a2c24dafc82cf8145421902750a05a6831f258f3
stanleyguo0207 <stanleyguo0207@163.com>
2022-04-11 00:04:25 +0800

    fix rocky-8.5 error. fix:    1. fix focky-8.5 error.
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- tools/init_dev.sh
+++ tools/init_dev.sh
@@ -3,6 +3,10 @@
 ScriptDir=`dirname $(readlink -f "$0")`
 echo "script directory: $ScriptDir"
 
+# core
+echo "core pattern set success."
+echo "core-%e-%p-%t" > /proc/sys/kernel/core_pattern
+
 # vscode
 cd $ScriptDir/vscode
 echo "vscode IN"
