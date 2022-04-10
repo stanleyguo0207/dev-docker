65aa6edb280bac8d218535159f921ef0dc593dcf
stanleyguo0207 <stanleyguo0207@163.com>
2022-04-10 13:35:06 +0800

    rocky-8.5 vscode settings update. feat:     1. rocky-8.5 vscode settings update.
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

---
+++ tools/vscode/install-extension.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension amiralizadeh9480.cpp-helper
+code --install-extension austin.code-gnu-global
+code --install-extension ccls-project.ccls
+code --install-extension ms-ceintl.vscode-language-pack-zh-hans
+code --install-extension xaver.clang-format
+code --install-extension cschlosser.doxdocgen
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension bierner.markdown-preview-github-styles
+code --install-extension ms-python.python
+code --install-extension gruntfuggly.todo-tree
+code --install-extension bbenoist.togglehs
+code --install-extension zxh404.vscode-proto3
+code --install-extension jeff-hykin.better-cpp-syntax
