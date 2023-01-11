e13025406489828e946b01379e6777be5cd86cf3
stanley <guotongshuai@ztgame.com>
2023-01-11 17:47:47 +0800

    feat(debian-11.6): Dockerfile update.

--- debian-11.6/build.sh
+++ debian-11.6/build.sh
@@ -9,7 +9,7 @@ fi
 docker login
 docker pull hello-world:latest
 docker pull debian:11.6
-docker build --network=host --no-cache -f Dockerfile -t $1 ../
+docker build --network=host --no-cache -f Dockerfile -t $1 .
 
 # ./build.sh dev_debian:v11.6
 # docker run -it dev_debian:v11.6 /bin/bash
