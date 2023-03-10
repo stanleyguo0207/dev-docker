19e1c26a543454398be00ee9a1b95f46d073e6ea
stanley <guotongshuai@ztgame.com>
2023-03-11 04:02:40 +0800

    feat: 脚本修改
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- debian-11.6/build.ps1
+++ debian-11.6/build.ps1
@@ -1,5 +1,3 @@
-# build.ps1 -image "image_name:tag_name"
-
 param(
     [string]$image = ""
 )
@@ -13,3 +11,5 @@ docker login
 docker pull hello-world:latest
 docker pull debian:11.6
 docker build --network=host --no-cache -f Dockerfile -t $image .
+
+# build.ps1 -image "dev_debian:v11.6"
