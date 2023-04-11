c869765a5de6d4537d9a03323294cf85768de6c5
stanley <guotongshuai@ztgame.com>
2023-04-11 18:58:53 +0800

    feat: 更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

---
+++ gamemaster-node/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+  echo "Usage: build.sh [image_name:tag_name]"
+  exit 1
+fi
+
+docker login
+docker pull hello-world:latest
+docker pull node:18.15-bullseye
+docker build --network=host --no-cache -f Dockerfile -t $1 .
+
+# ./build.sh gamemaster:1.0
+# docker run -it gamemaster:1.0 /bin/bash
