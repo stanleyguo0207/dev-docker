8610f65a73d90af817f09f85a2de0cc5d0493a58
stanleyguo0207 <guotongshuai@ztgame.com>
2023-04-13 21:37:38 +0800

    feat: gamemaster镜像更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- gamemaster/createcontainer.ps1
+++ gamemaster/createcontainer.ps1
@@ -1,8 +1,7 @@
 param(
     [string]$image = "",
     [string]$name = "",
-    [int]$port = 9901,
-    [string]$host_port_range = ""
+    [int]$port = 9901
 )
 
 if ([string]::IsNullOrEmpty($image) -or [string]::IsNullOrEmpty($name)) {
@@ -10,47 +9,13 @@ if ([string]::IsNullOrEmpty($image) -or [string]::IsNullOrEmpty($name)) {
     exit -1
 }
 
-if ([string]::IsNullOrEmpty($host_port_range)) {
-    $port_range = ""
-}
-else {
-    $container_port_start = 19900
-    $container_port_max = 21
-
-    $host_port_array = $host_port_range -split "-"
-    if (2 -ne $host_port_array.Length) {
-        Write-Host "Error: the range must be two numbers, host $host_port_array"
-        exit -1
-    }
-    $host_port_start = $host_port_array[0] -as [int]
-    $host_port_max = ($host_port_array[1] -as [int]) - $host_port_start + 1
-    if ($host_port_max -gt $container_port_max) {
-        Write-Host "Error: only supports opening up to 20 ports, host $host_port_range"
-        exit -1
-    }
-
-    $port_range = ""
-    for ($i = 0; $i -lt $host_port_max; $i++) {
-        $tmp_host_port = $host_port_start + $i
-        $tmp_container_port = $container_port_start + $i
-        $port_range += "-p $tmp_host_port`:$tmp_container_port "
-    }
+function CreateContainer($_image, $_name, $_port) {
+  docker create --name `"$_name`" -p `"$_port`":22 --security-opt seccomp=unconfined --privileged=true --restart=always `"$_image`" /sbin/init
+  docker start $_name
+  Write-Host "create root:root accounts for container $_image $_name port:$_port"
+  Write-Host "create $_name`:$_name accounts for container $_image $_name port:$_port"
 }
 
-$script_dir = Get-Location
-$tools_dir = "$script_dir\..\tools"
+CreateContainer $image $name $port
 
-function CreateContainer($_image, $_name, $_port, $_port_range) {
-    docker create --name `"$_name`"_dvc -v /opt/docker_home hello-world:latest
-    docker create --name `"$_name`" --volumes-from `"$_name`"_dvc -p `"$_port`":22 $_port_range.Split() --security-opt seccomp=unconfined --privileged=true --restart=always `"$_image`" /sbin/init
-    docker start $_name
-    docker cp $script_dir\createuser.sh ${_name}:/root
-    docker cp $tools_dir ${_name}:/opt/docker_home
-    docker exec -it $_name /bin/bash /root/createuser.sh $_name $_port
-    Write-Host "create root:root accounts for container $_image $_name port:$_port"
-    Write-Host "create $_name`:$_name accounts for container $_image $_name port:$_port"
-}
-
-CreateContainer $image $name $port $port_range
-
-# createcontainer.ps1 -image "gamemaster:1.0" -name "stanley" -port 9901 -host_port_range "19900-19905"
+# createcontainer.ps1 -image "gamemaster:1.0" -name "stanley" -port 9901
--- gamemaster/createcontainer.sh
+++ gamemaster/createcontainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 3 ]; then
-  echo "Usage: createcontainer.sh [image_name:tag_name] [container_name] [port] [host_port_range]"
+  echo "Usage: createcontainer.sh [image_name:tag_name] [container_name] [port]"
   exit -1
 fi
 
@@ -15,44 +15,15 @@ tools_dir="$script_dir/../tools"
 image="$1"
 name="$2"
 port=$3
-host_port_range="$4"
-port_range=""
-
-if [ $host_port_range ]; then
-  container_port_start=19900
-  container_port_max=21
-
-  host_port_array=(${host_port_range//"-"/" "})
-  host_port_array_len=${#host_port_array[@]}
-  if [ 2 -ne $host_port_array_len ]; then
-    echo "Error: the range must be two numbers, host ${host_port_array[@]}"
-    exit -1
-  fi
-  host_port_start=${host_port_array[0]}
-  host_port_max=$(expr ${host_port_array[1]} - $host_port_start)
-  if [ $host_port_max -gt $container_port_max ]; then
-    echo "Error: only supports opening up to 20 ports, host $host_port_range"
-    exit -1
-  fi
-  port_range=""
-  for i in $(seq 0 $host_port_max); do
-    tmp_host_port=$(expr $host_port_start + $i)
-    tmp_container_port=$(expr $container_port_start + $i)
-    port_range="$port_range-p $tmp_host_port:$tmp_container_port "
-  done
-fi
 
 function CreateContainer() {
   docker create --name "$2"_dvc -v /opt/docker_home hello-world:latest
-  docker create --name "$2" --volumes-from "$2"_dvc -p "$3":22 $4 --security-opt seccomp=unconfined --privileged=true --restart=always "$1" /sbin/init
+  docker create --name "$2" --volumes-from "$2"_dvc -p "$3":22 --security-opt seccomp=unconfined --privileged=true --restart=always "$1" /sbin/init
   docker start $2
-  docker cp $script_dir/createuser.sh $2:/root
-  docker cp $tools_dir $2:/opt/docker_home
-  docker exec -it $2 /bin/bash /root/createuser.sh $2 $3
   echo "create root:root accounts for container $1 $2 port:$3"
   echo "create $2:$2 accounts for container $1 $2 port:$3"
 }
 
-CreateContainer $image $name $port "${port_range[@]}"
+CreateContainer $image $name $port
 
-# ./createcontainer.sh gamemaster:1.0 stanley 9901 19900-19920
+# ./createcontainer.sh gamemaster:1.0 stanley 9901
